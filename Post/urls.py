af80e025afdefeed2ef6c2e6f5b6f9e3f9995d3e
kmg0485 <kmg0485@gmail.com>
2022-10-17 22:57:21 +0900

    amend result.html and main.html, Add search_view

--- Post/urls.py
+++ Post/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('post_view/<int:id>/delete/',views.delete_post, name='delete-post'),
     path('post_view/<int:id>/edit/',views.edit_post, name='edit-post'),
     path("upload/", views.upload_img, name="upload_img"),
+    path("search/", views.search_view, name="search"),
+
     # path("detail/<int:pk>/", views.detail_post, name="detail_post"),
     ]
