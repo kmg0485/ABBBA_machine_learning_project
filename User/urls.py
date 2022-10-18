256781d76e90933f82e8c60a4a9d23b359cead03
MINKYUOH <dhalsrbb123@gmail.com>
2022-10-18 11:28:50 +0900

    signup
    signup 페이지 추가

--- User/urls.py
+++ User/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from User import views
+
+app_name = "User"
+
 urlpatterns = [
-   
-   
     
+    path('signup/', views.signup, name='signup'),
 ]
