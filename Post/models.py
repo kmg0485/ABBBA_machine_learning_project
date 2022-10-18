f17cf34c6d4dcd1e27d2400b54001742900efde2
GeumBinLee <lgb9811@gmail.com>
2022-10-18 11:35:55 +0900

    Add: Comment function

--- Post/models.py
+++ Post/models.py
@@ -1,4 +1,5 @@
 # 모델링
+from unittest.util import _MAX_LENGTH
 from django.db import models
 # from user.models import UserModel
 
@@ -7,14 +8,14 @@ import os
 from uuid import uuid4
 from django.utils import timezone
 
-# 태그
-#from taggit.managers import TaggableManager
+#태그
+from taggit.managers import TaggableManager
 
 class PostModel(models.Model) :
     content = models.TextField()
-    # author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    # tags = TaggableManager(blank=True)
+    tags = TaggableManager(blank=True)
    
     def upload_img(instance, filename):
         # upload_to="%Y/%m/%d" 처럼 날짜로 세분화
@@ -34,8 +35,11 @@ class PostModel(models.Model) :
 
     photo = models.ImageField(upload_to=upload_img)
 
-    '''def __str__(self):
-        return str(F'{self.author.username}의 글 내용 : {self.content}')'''
-    # 추후 해시태그 필드 추가
-# Create your models here.
+    def __str__(self):
+        return str(F'{self.author.username}의 글 내용 : {self.content}')
 
+class CommentModel(models.Model) :
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
