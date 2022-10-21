3f8c34c74ad7edc655fc1c5b5c6969c6a3816d86
GeumBinLee <lgb@lgb.local>
2022-10-21 12:51:42 +0900

    Feature: search with tags

--- Post/tests.py
+++ Post/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+
+def flatten(lst) :
+    result = []
+    for item in lst :
+        if type(item) == list :
+            result += flatten(item)
+        else :
+            result += [item]
+    return result
 # Create your tests here.
+a = [[1,2,3]]
+b = flatten(a)
+
+print(b)
