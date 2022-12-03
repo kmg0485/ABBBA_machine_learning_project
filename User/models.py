8a8708914f46b32b623ab0924b0af1078b5b29e6
kmg0485 <kmg0485@naver.com>
2022-12-03 15:36:26 +0900

    :hammer:Fix: some codes

--- User/models.py
+++ User/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from Post.models import PostModel
 
-# Create your models here.
 class UserModel(AbstractUser):
     nickname = models.CharField(max_length=500, blank=True)
     like_posts = models.ManyToManyField('Post.PostModel', blank=True, related_name='like_users',)
